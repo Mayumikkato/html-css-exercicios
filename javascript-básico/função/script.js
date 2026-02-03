d89c3f11deec52b925f5d6ae50b3403632136e5d
Fernanda Mayumi <128170082+Mayumikkato@users.noreply.github.com>
2026-02-03 13:38:00 -0300

    adiciona estudos de função em JavaScript

---
+++ javascript-básico/função/script.js
@@ -0,0 +1,72 @@
+//---------------- Isso é a mesma coisa:
+function logCurso() {
+  const nome = "JavaScript";
+  console.log(nome);
+}
+
+logCurso();
+//----------------- que isso:
+function logCurso(nome) {
+  console.log(nome);
+}
+
+logCurso("JavaScript");
+//------------------
+
+//--- Função usando return
+const nome = "JavaScript";
+
+function logCurso() {
+  const nome = "HTML";
+  console.log(nome);
+  return nome;
+}
+
+const nomeDaFuncao = logCurso();
+
+console.log(nomeDaFuncao);
+console.log(nome);
+//-------------
+
+
+//-----Exercício:
+// 1 - Crie uma função (livro) que possui 3 parâmetros: nome, ano e autor.
+// 2 - No corpo da função:
+/// 2.1 - Transforme o nome para letra maiúscula: toUpperCase()
+/// 2.2 - Calcule o total de anos desde o lançamento do livro: 2050 - ano
+/// 2.3 - Crie uma variável com a frase: nome + ' por ' + autor;
+/// 2.4 - Coloque os 3 valores acima em um objeto.
+// 3 - Retorn (return) o objeto definido
+// 4 - Execute a função com os seguintes argumentos: 'O Senhor dos Anéis', 1954, 'J. R. R. Tolkien'
+// 5 - Guarde o retorno da função executada em uma variável.
+// 6 - Log a frase final da função executada no console.
+
+
+///-----RESOLUÇÃO mais descritiva:
+// function livro(nome, ano, autor) {
+//   const nomeMaior = nome.toUpperCase();
+//   const totalAnos = 2050 - ano;
+//   const frase = nome + " por " + autor;
+//   const objeto = {
+//     nome: nomeMaior,
+//     totalAnos,
+//     frase,
+//   };
+//   return objeto;
+// }
+
+//const livroRetorno = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
+//console.log(livroRetorno);
+
+
+///----- RESOLUÇÃO mais simplificada:
+function livro(nome, ano, autor) {
+  return {
+    nome: nome.toUpperCase(),
+    totalAnos: 2050 - ano,
+    frase: nome + " por " + autor,
+  };
+}
+
+const livroRetorno = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
+console.log(livroRetorno);
